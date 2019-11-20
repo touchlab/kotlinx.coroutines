d2a012a3bdab40b2bb1bc97c807070c937b49139
Roman Elizarov <elizarov@gmail.com>
2019-11-20 17:52:23 +0300

    Property support exception cause on JS and Native

--- kotlinx-coroutines-core/native/src/Exceptions.kt
+++ kotlinx-coroutines-core/native/src/Exceptions.kt
@@ -9,30 +9,17 @@ import kotlinx.coroutines.internal.*
 import kotlin.native.ref.*
 
 /**
- * This exception gets thrown if an exception is caught while processing [CompletionHandler] invocation for [Job].
- *
- * @suppress **This an internal API and should not be used from general code.**
- */
-@InternalCoroutinesApi
-public actual class CompletionHandlerException public actual constructor(
-    message: String,
-    public override val cause: Throwable
-) : RuntimeException(message.withCause(cause))
-
-/**
  * Thrown by cancellable suspending functions if the [Job] of the coroutine is cancelled while it is suspending.
  * It indicates _normal_ cancellation of a coroutine.
  * **It is not printed to console/log by default uncaught exception handler**.
  * (see [CoroutineExceptionHandler]).
  */
-public actual open class CancellationException actual constructor(message: String?) :   IllegalStateException(message)
-
-/**
- * Creates a cancellation exception with a specified message and [cause].
- */
-@Suppress("FunctionName")
-public actual fun CancellationException(message: String?, cause: Throwable?) : CancellationException =
-    CancellationException(message.withCause(cause))
+public actual open class CancellationException(
+    message: String?,
+    cause: Throwable?
+) : IllegalStateException(message, cause) {
+    actual constructor(message: String?) : this(message, null)
+}
 
 /**
  * Thrown by cancellable suspending functions if the [Job] of the coroutine is cancelled or completed
@@ -41,9 +28,9 @@ public actual fun CancellationException(message: String?, cause: Throwable?) : C
  */
 internal actual class JobCancellationException public actual constructor(
     message: String,
-    public override val cause: Throwable?,
+    cause: Throwable?,
     job: Job
-) : CancellationException(message.withCause(cause)) {
+) : CancellationException(message, cause) {
     private val ref = WeakReference(job)
     internal actual val job: Job?
         get() = ref.get()
@@ -55,17 +42,6 @@ internal actual class JobCancellationException public actual constructor(
     override fun hashCode(): Int =
         (message!!.hashCode() * 31 + job.hashCode()) * 31 + (cause?.hashCode() ?: 0)
 }
-
-@Suppress("FunctionName")
-internal fun IllegalStateException(message: String, cause: Throwable?) =
-    IllegalStateException(message.withCause(cause))
-
-private fun String?.withCause(cause: Throwable?) =
-    when {
-        cause == null -> this
-        this == null -> "caused by $cause"
-        else -> "$this; caused by $cause"
-    }
 
 internal actual fun Throwable.addSuppressedThrowable(other: Throwable) {
     if (this is SuppressSupportingThrowableImpl) addSuppressed(other)
